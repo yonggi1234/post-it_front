6e65717045fb796afd7e9e6ec3cf6fd1d16e4436
yonggi1234 <littedeer108@gmail.com>
2023-06-12 21:52:00 +0900

    no message

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const ejs = require('ejs')
-// var db_config = require(__dirname + '/config/database.js');
-// 포트번호 3000
+const ejs = require('ejs');
+
+const router = express.Router();
+
+
 const port = 3000;
 
 const path = require('path');
@@ -42,14 +44,16 @@ const connection = mysql.createConnection({
 //     });
 // connection.end(); 
 
-
+//kanban 호출
 app.get('/kanban', function (req, res) {
-    var sql ="SELECT img FROM post";
+    var sql ="SELECT * FROM post";
     connection.query(sql, function (err, result, fields) {
         if(err) console.log(err);
         else res.render('kanban', {data : result});
     });
 });
+
+//pmi 호출
 app.get('/pmi', function (req, res) {
     var sql ="SELECT img FROM post";
     connection.query(sql, function (err, result, fields) {
@@ -57,26 +61,62 @@ app.get('/pmi', function (req, res) {
         else res.render('pmi', {data : result});
     });
 });
-  app.get('/api/postits', function (req, res) {
-    var sql = "SELECT img, position_x, position_y FROM post";
-    connection.query(sql, function (err, result, fields) {
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-      } else {
-        // 가져온 데이터를 기반으로 이미지를 생성하고 보드에 추가하는 로직 작성
-        var postits = result.map(postit => {
-          return {
-            img: postit.img,
-            position_x: postit.position_x,
-            position_y: postit.position_y
-          };
-        });
-        res.json(postits);
-      }
+
+
+app.get('/api/postits', function (req, res) {
+  var sql = "SELECT img, position_x, position_y FROM post";
+  connection.query(sql, function (err, result, fields) {
+    if (err) {
+    console.log(err);
+    res.sendStatus(500);
+    } else {
+    // 가져온 데이터를 기반으로 이미지를 생성하고 보드에 추가하는 로직 작성
+    var postits = result.map(postit => {
+      return {
+        img: postit.img,
+        position_x: postit.position_x,
+        position_y: postit.position_y
+      };
     });
+    res.json(postits);
+    }
   });
+});
   
+// var save=document.getElementById("save");
+
+// save.addEventListener('click',function(){
+//     var postDIR=document.querySelectorAll('[class*=img]')
+//     postDIR.forEach(function(post) {
+//         var postedit = 
+//         {
+//             img: post.className,
+//             position_x: getComputedStyle(post).left,
+//             position_y: getComputedStyle(post).top,
+//         };
+//         console.log(postedit);
+//       });
+//     });
+
+app.get("/api/save", (req, res) => {
+    //수정하려는 포스트잇 정보 불러오기
+        console.log(res);
+
+        // var sql= 'UPDATE post SET position_x=?, position_y=? WHERE img=?';
+        // connection(sql,[img,x,y],function(err,result,fields){
+        //       if(err){
+        //         console.log(err);
+        //         res.sendStatus(500);
+        //       }
+        //       else  {
+        //         console.log("성공")
+        //         res.redirect('/')
+        //       }
+        //     })
+});
+
+
+
 app.get('/scammper', function (req, res) {
     var sql ="SELECT img FROM post";
     connection.query(sql, function (err, result, fields) {
@@ -86,10 +126,6 @@ app.get('/scammper', function (req, res) {
 });
 
 
-//파일 업로드
-const indexRouter = require('./server');
-app.set('port', process.env.PORT || 8005);
-app.use('/user', indexRouter);
 
 
 
