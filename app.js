a5a1d9a3229c21f45c983de04b2b95f2e9c1f75b
yonggi1234 <littedeer108@gmail.com>
2023-06-14 18:32:54 +0900

    no message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const ejs = require('ejs');
 
-const router = express.Router();
+// const router = express.Router();
 
 
 const port = 3000;
@@ -35,18 +35,10 @@ const connection = mysql.createConnection({
   database : 'post_it'
 });
 
-//db값 확인
-// connection.connect();
-// let sql ="SELECT img FROM post";
-// connection.query(sql, function (err, rows, fields) { 
-//         if (err)    console.log(err);
-//         else console.log(rows[1]);
-//     });
-// connection.end(); 
 
 //kanban 호출
 app.get('/kanban', function (req, res) {
-    var sql ="SELECT * FROM post";
+    var sql ="SELECT * FROM post where img like '/img/pmi%'";
     connection.query(sql, function (err, result, fields) {
         if(err) console.log(err);
         else res.render('kanban', {data : result});
@@ -55,16 +47,25 @@ app.get('/kanban', function (req, res) {
 
 //pmi 호출
 app.get('/pmi', function (req, res) {
-    var sql ="SELECT img FROM post";
+    var sql ="SELECT * FROM post where img like '/img/pmi%'";
     connection.query(sql, function (err, result, fields) {
         if(err) console.log(err);
         else res.render('pmi', {data : result});
     });
 });
 
+//scammper 호출
+app.get('/scammper', function (req, res) {
+  var sql ="SELECT * FROM post where img like '/img/scammper%'";
+  connection.query(sql, function (err, result, fields) {
+      if(err) console.log(err);
+      else res.render('scammper', {data : result});
+  });
+});
 
-app.get('/api/postits', function (req, res) {
-  var sql = "SELECT img, position_x, position_y FROM post";
+
+app.get('/api/pmi', function (req, res) {
+  var sql = "SELECT img, position_x, position_y,size FROM post where img like '/img/pmi%'";
   connection.query(sql, function (err, result, fields) {
     if (err) {
     console.log(err);
@@ -75,7 +76,29 @@ app.get('/api/postits', function (req, res) {
       return {
         img: postit.img,
         position_x: postit.position_x,
-        position_y: postit.position_y
+        position_y: postit.position_y,
+        size: postit.size
+      };
+    });
+    res.json(postits);
+    }
+  });
+});
+
+app.get('/api/scammper', function (req, res) {
+  var sql = "SELECT img, position_x, position_y, size FROM post where img like '/img/scammper%'";
+  connection.query(sql, function (err, result, fields) {
+    if (err) {
+    console.log(err);
+    res.sendStatus(500);
+    } else {
+    // 가져온 데이터를 기반으로 이미지를 생성하고 보드에 추가하는 로직 작성
+    var postits = result.map(postit => {
+      return {
+        img: postit.img,
+        position_x: postit.position_x,
+        position_y: postit.position_y,
+        size: postit.size
       };
     });
     res.json(postits);
@@ -83,50 +106,25 @@ app.get('/api/postits', function (req, res) {
   });
 });
   
-// var save=document.getElementById("save");
 
-// save.addEventListener('click',function(){
-//     var postDIR=document.querySelectorAll('[class*=img]')
-//     postDIR.forEach(function(post) {
-//         var postedit = 
-//         {
-//             img: post.className,
-//             position_x: getComputedStyle(post).left,
-//             position_y: getComputedStyle(post).top,
-//         };
-//         console.log(postedit);
-//       });
-//     });
-
-app.get("/api/save", (req, res) => {
-    //수정하려는 포스트잇 정보 불러오기
-        console.log(res);
-
-        // var sql= 'UPDATE post SET position_x=?, position_y=? WHERE img=?';
-        // connection(sql,[img,x,y],function(err,result,fields){
-        //       if(err){
-        //         console.log(err);
-        //         res.sendStatus(500);
-        //       }
-        //       else  {
-        //         console.log("성공")
-        //         res.redirect('/')
-        //       }
-        //     })
-});
-
-
-
-app.get('/scammper', function (req, res) {
-    var sql ="SELECT img FROM post";
-    connection.query(sql, function (err, result, fields) {
-        if(err) console.log(err);
-        else res.render('scammper', {data : result});
-    });
-});
-
-
-
+// app.get("/api/save", (req, res) => {
+//     img=res.img;
+//     x=res.position_x;
+//     y=res.position_y;
+//     //수정하려는 포스트잇 정보 불러오기
+//         var sql= 'SELECT * from post WHERE position_x=? and position_y=? and img=?';
+//         // var sql= 'UPDATE post SET position_x=?, position_y=? WHERE img=?';
+//         connection(sql,[img,x,y],function(err,result,fields){
+//               if(err){
+//                 console.log(err);
+//                 res.sendStatus(500);
+//               }
+//               else  {
+//                 console.log("성공")
+//                 res.redirect('/kanban')
+//               }
+//             })
+// });
 
 
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 // routes/user.js
-const express = require('express');
+const express = require("express");
+const app = express();
+const ejs = require('ejs')
 
-const router = express.Router();
+// app.get("/api/save", (req, res) => {
+  //수정하려는 포스트잇 정보 불러오기
+      // console.log(res);
 
-router.get('/upload', function(req, res){
-  res.render('upload');
-});
-
-router.post('/upload', function(req,res){
-    res.send("성공")
-});
-
-module.exports = router;
+      // var sql= 'UPDATE post SET position_x=?, position_y=? WHERE img=?';
+      // connection(sql,[img,x,y],function(err,result,fields){
+      //       if(err){
+      //         console.log(err);
+      //         res.sendStatus(500);
+      //       }
+      //       else  {
+      //         console.log("성공")
+      //         res.redirect('/')
+      //       }
+      //     })
+// });
